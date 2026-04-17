67701600f4a191aa6fdc6d9ee80a3e0dda07761f
Marcelo Dompieri <marcelofdompieri@gmail.com>
2026-04-17 09:58:13 -0300

    Add user_config_override.h for configuration settings

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,19 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+#ifdef USE_SCD40
+#undef USE_SCD40
+#endif
+#define USE_SCD40
+
+#ifdef USE_SPI
+#undef USE_SPI
+#endif
+#define USE_SPI
+
+#ifdef USE_SDCARD
+#undef USE_SDCARD
+#endif
+#define USE_SDCARD
+
+#endif
